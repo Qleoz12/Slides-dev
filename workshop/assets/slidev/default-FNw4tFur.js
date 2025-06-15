3ca144455e223c7be81289631fd4d0cc9f1b300d
Qleoz12 <Qleoz12@users.noreply.github.com>
2025-06-15 01:16:18 +0000

    Deploy Slidev to GitHub Pages fba44140f07328f7570614558feff76cb71d303d

---
+++ workshop/assets/slidev/default-FNw4tFur.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-i6Kn5-TQ.js";import{_ as r}from"../index-B3i8jH81.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
